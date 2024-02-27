be85895c9a3488df093a950fe1f4158bbcf23b1c
DaniIliev <114102749+DaniIliev@users.noreply.github.com>
2024-02-27 20:12:11 +0200

    add some css

--- app/src/components/pages/Login.jsx
+++ app/src/components/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import { useForm } from "../../hooks/useForm";
 import { useAuthContext } from "../../context/authContext";
@@ -12,14 +13,16 @@ const Login = () => {
   const {login} = useAuthContext()
   const {values, changeHandler, submit} = useForm(initialFormValues, login)
   return (
+    <>
     <div className="loginRegisterForm">
+    <img src="/icons/brain2.png" alt="brain" className="backgroundImg"/>
       <Form className="form" onSubmit={submit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" name="email" onChange={changeHandler} value={values.email}/>
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          {/* <Form.Text className=""> */}
+            <p className="pText">We'll never share your email with anyone else.</p>
+          {/* </Form.Text> */}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -28,9 +31,15 @@ const Login = () => {
         </Form.Group>
         <div className="submitBtnWrp">
           <Button variant="primary" type="submit" className="btnSubmit">Login</Button>
+          <div className="switchLoginRegisterPage">
+              <p>Don't have an account?</p>
+              <Link to={'/register'}>Register here...</Link>
+          </div>
         </div>
       </Form>
     </div>
+    </>
+
   );
 };
 
--- app/src/components/pages/Register.jsx
+++ app/src/components/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Form, Button } from 'react-bootstrap'
 import { useForm } from '../../hooks/useForm';
+import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../context/authContext';
 
 const initialFormValues = {
@@ -15,20 +16,22 @@ const Register = () => {
 
   return (
     <div className="loginRegisterForm">
+    <img src="/icons/brain2.png" alt="brain" className="backgroundImg"/>
+
       <Form className="form" onSubmit={submit}>
         <Form.Group className="mb-3" controlId="formBasicUsername">
           <Form.Label>Username</Form.Label>
           <Form.Control name='username' type="text" placeholder="Enter username" onChange={changeHandler} value={values.username}/>
-          <Form.Text className="text-muted">
+          <p className="pText">
             Everyone else will see you with this name.
-          </Form.Text>
+          </p>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control name='email' type="email" placeholder="Enter email" onChange={changeHandler} value={values.email}/>
-          <Form.Text className="text-muted">
+          <p className="pText">
             We'll never share your email with anyone else.
-          </Form.Text>
+          </p>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -37,6 +40,10 @@ const Register = () => {
         </Form.Group>
         <div className="submitBtnWrp">
           <Button variant="primary" type="submit" className="btnSubmit">Register</Button>
+          <div className="switchLoginRegisterPage">
+              <p>Already have an account?</p>
+              <Link to={'/login'}>Login here...</Link>
+          </div>
         </div>
       </Form>
     </div>
